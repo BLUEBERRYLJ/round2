86a3fac26c6d3894f0c1cdc1b0cd005a05f1255b
tanggu01 <tanggu1221@gmail.com>
2022-12-04 18:12:20 +0900

    Add community related entity

--- src/main/java/com/round2/round2/src/domain/Member.java
+++ src/main/java/com/round2/round2/src/domain/Member.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -22,13 +25,53 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-
-    private String nickname;
+    private String name;
     private String email; //regex 추가
     private String pwd; //regex 추가
 
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "enum")
+    private Status status;
     private String role;
 
+
+    @OneToMany(mappedBy = "member")
+    private List<Post> postList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<PostLike> postLikeList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Comment> commentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Course> courseList = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+
+    //== 생성 메서드 ==// -> constructor 역할.
+    public static Member createMember(String name, String email, String pwd) {
+        Member member = new Member();
+        member.setName(name);
+        member.setEmail(email);
+        member.setPwd(pwd);
+        member.createdAt = LocalDateTime.now();
+        member.updatedAt = LocalDateTime.now();
+        member.status = Status.ACTIVE;
+        member.role = "UNSUBSCRIBED";
+        return member;
+    }
+
+
+    public void deleteMember() {
+        this.deletedAt = LocalDateTime.now();
+        this.status = Status.INACTIVE;
+
+    }
 }
+
 
 
---
+++ src/main/java/com/round2/round2/src/domain/PostLike.java
@@ -0,0 +1,53 @@
+package com.round2.round2.src.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="post_like")
+
+public class PostLike {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post Post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+//    public static PostLike likesPost(Post Post, Member member) {
+//        List<PostLike> PostLikeList = member.getPostLikeList();
+//        if (PostLikeList == null || PostLikeList.isEmpty()) {
+//        }
+//        else {
+//            for (PostLike PostLike : PostLikeList) {
+//                if (Objects.equals(PostLike.getPost().getId(), Post.getId())) {
+//                    return null;
+//                }
+//            }
+//        }
+//        PostLike PostLike = new PostLike();
+//        PostLike.setMember(member);
+//        PostLike.setPost(Post);
+//        PostLike.createdAt = LocalDateTime.now();
+//        return PostLike;
+//    }
+
+}
