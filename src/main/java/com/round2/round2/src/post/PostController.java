f32828cbca7367eb4a4c00346dc195f0e80a5348
tanggu01 <tanggu1221@gmail.com>
2022-12-05 22:58:11 +0900

    Exceptionhandler ING

--- src/main/java/com/round2/round2/src/post/PostController.java
+++ src/main/java/com/round2/round2/src/post/PostController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Basic;
 
 @RestController
 @RequestMapping("/post")
@@ -20,27 +19,42 @@ public class PostController {
 
     /**
      * 3.3 게시물 생성
+     * Error v1 - 그냥 아이디어
+     */
+//    @PostMapping
+//    public ResponseEntity<BasicResponse> createPost(@RequestBody CreatePostRequest request) {
+//        if (request.getContent().isEmpty() || request.getTitle().isEmpty()) { // 제목 / 본문 비어있을때
+//            return new ResponseEntity<>(new BasicResponse("게시물 제목/본문을 입력해주세요."), HttpStatus.BAD_REQUEST);
+//        }
+//        PostResponse postResponse = postService.createPost(request);
+//        if (postResponse == null) { //cannot create - server error
+//            return new ResponseEntity<>(new BasicResponse("Server error"),HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//        return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
+//    }
+
+    /**
+     * 3.3 게시물 생성
+     * Error v2
      */
     @PostMapping
     public ResponseEntity<BasicResponse> createPost(@RequestBody CreatePostRequest request) {
         if (request.getContent().isEmpty() || request.getTitle().isEmpty()) { // 제목 / 본문 비어있을때
             return new ResponseEntity<>(new BasicResponse("게시물 제목/본문을 입력해주세요."), HttpStatus.BAD_REQUEST);
         }
-
         PostResponse postResponse = postService.createPost(request);
         if (postResponse == null) { //cannot create - server error
             return new ResponseEntity<>(new BasicResponse("Server error"),HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-//        return ResponseEntity.created().body(postResponse);
         return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
     }
-//
-//
-//     return new ResponseEntity<MoveResponseDto>(moveResponseDto, headers, HttpStatus.valueOf(200));
-//
-//  return ResponseEntity.ok()
-//          .headers(headers)
-//        .body(moveResponseDto);
+
+
+    @ExceptionHandler(NullPointerException.class)
+    public Object nullex(Exception e) {
+        System.err.println(e.getClass());
+        return "myService";
+    }
+
 
 }
