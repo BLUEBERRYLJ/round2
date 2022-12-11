bfd1b85c3ad6f26346761b607791ab13427b42f7
LeoJung <leojung0909@gmail.com>
2022-12-11 12:29:14 +0900

    jwtauthenticationfilter add role

---
+++ src/main/java/com/round2/round2/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.round2.round2.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+
+public class SwaggerConfig {
+
+    /**
+     * Security 적용
+     */
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String version) {
+        Info info = new Info()
+                .title("Mingle API ") // 타이틀
+                .version(version) // 문서 버전
+                .description("Round 2 프로젝트 API 명세서 입니다."); // 문서 설명
+
+
+        // Security 스키마 설정
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("JWT");
+
+        return new OpenAPI()
+                // Security 인증 컴포넌트 설정
+                .components(new Components().addSecuritySchemes("JWT", bearerAuth))
+                // API 마다 Security 인증 컴포넌트 설정
+                .addSecurityItem(addSecurityItem)
+                .info(info);
+    }
+
+
+}
--- src/main/java/com/round2/round2/src/comment/CommentController.java
+++ src/main/java/com/round2/round2/src/comment/CommentController.java
@@ -5,11 +5,21 @@ import com.round2.round2.config.exception.ErrorCode;
 import com.round2.round2.config.exception.ErrorResponse;
 import com.round2.round2.src.comment.model.PostCommentRequest;
 import com.round2.round2.src.comment.model.PostCommentResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@Tag(name = "comment", description = "댓글 관련 API")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content (schema = @Schema(hidden = true))),
+        @ApiResponse(responseCode = "403", description = "토큰을 입력해주세요 (Bearer 제외).", content = @Content(schema = @Schema(hidden = true))),
+})
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/comment")
@@ -21,6 +31,12 @@ public class CommentController {
     /**
      * 4.1 댓글 작성
      */
+    @Operation(summary = "4.1 댓글 작성 api", description = "4.1 댓글 작성 api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "401", description = "code:1001 | 유저를 찾지 못했습니다", content = @Content (schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "406", description = "code:3005 | 게시물을 찾지 못했습니다", content = @Content (schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "406", description = "code:4001 | 댓글 생성에 실패했습니다", content = @Content (schema = @Schema(hidden = true)))
+    })
     @PostMapping
     public ResponseEntity<PostCommentResponse> createComment(@RequestBody PostCommentRequest postCommentRequest) {
         PostCommentResponse postCommentResponse = commentService.createComment(postCommentRequest);
@@ -32,6 +48,13 @@ public class CommentController {
      * @param id
      * @return
      */
+    @Operation(summary = "4.2 댓글 삭제 api", description = "4.1 댓글 삭제 api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "406", description = "code:4002 | 댓글을 찾지 못했습니다.", content = @Content (schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", description = "code:4003 | 댓글 수정/삭제에 실패했습니다.", content = @Content (schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "code:5001 | 데이터베이스 에러", content = @Content (schema = @Schema(hidden = true)))
+
+    })
     @PatchMapping("/status/{id}")
     public ResponseEntity<String> deleteComment(@PathVariable Long id) {
         commentService.deleteComment(id);
